220490924259b60b37ecdd1b9421ee5061d4c5b2
Miqhtie <60279933+Miqhtiedev@users.noreply.github.com>
2020-10-10 00:01:18 -0400

    Put chatevent in its own file, create utils class to shove random methods in

---
+++ src/main/java/me/ryanarora/events/ChatEvent.java
@@ -0,0 +1,23 @@
+package me.ryanarora.events;
+
+import me.ryanarora.BoopBack;
+import me.ryanarora.utils.Utils;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+public class ChatEvent {
+
+    private final Minecraft mc = BoopBack.mc;
+
+    @SubscribeEvent
+    public void chat(ClientChatReceivedEvent event){
+        String text = event.message.getUnformattedText();
+
+        if (text.startsWith("From ") && text.endsWith(": Boop!")) {
+            String[] reversed = Utils.reverseStringArray(text.split(" "));
+            String username = reversed[1].replace(":", "");
+
+            mc.thePlayer.sendChatMessage("/boop " + username);
+        }
+    }
+}
